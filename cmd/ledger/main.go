5f643469469b3b8aae65a00fec7202d78fb90100
Assil Jaby <altf4irl@gmail.com>
2025-01-24 13:03:16 +0300

    refactor: add dev env loading

--- cmd/ledger/main.go
+++ cmd/ledger/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"github.com/assiljaby/go-ledger-backend/environments"
 	"github.com/assiljaby/go-ledger-backend/internal/router"
 	"github.com/assiljaby/go-ledger-backend/internal/server"
 	"github.com/assiljaby/go-ledger-backend/pkg/logger"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	logger.StartLogger()
 
-	_ = godotenv.Load()
+	environments.LoadEnv()
 
 	e := server.GetInstance()
 
--- environments/env.go
+++ environments/env.go
@@ -1,6 +1,18 @@
 package environments
 
-import "os"
+import (
+	"os"
+
+	"github.com/assiljaby/go-ledger-backend/pkg/logger"
+	"github.com/joho/godotenv"
+)
+
+// LoadEnv Loads env variables in development.
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		logger.Info("was not able to load .env, proceeding to load OS env")
+	}
+}
 
 func getenv(key string, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
